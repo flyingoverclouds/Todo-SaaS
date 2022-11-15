9bf72ca56b2844663ffa3d93c4b629db6d622b72
Nicolas CLERC <nicolas.clerc@sablefin.net>
2022-11-15 23:18:08 +0100

    source code init

---
+++ v1/src/todo-service/IItemDataService.cs
@@ -0,0 +1,13 @@
+﻿using todo_service.Models;
+
+namespace todo_service
+{
+    public interface IItemDataService
+    {
+        TodoItem GetItem(string Tenant, Guid todoId);
+        void CreateItem(TodoItem item);
+        void DeleteItem(string Tenant, Guid todoId);
+        void CheckItem(string Tenant, Guid todoId, bool done);
+
+    }
+}
---
+++ v1/src/todo-service/Services/TodoItemService.cs
@@ -0,0 +1,36 @@
+﻿using todo_service.Models;
+
+namespace todo_service.Services
+{
+    public class TodoItemService : IItemDataService
+    {
+        public void CheckItem(string Tenant, Guid todoId, bool done)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void CreateItem(TodoItem item)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void DeleteItem(string Tenant, Guid todoId)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public TodoItem GetItem(string Tenant, Guid todoId)
+        {
+            return new TodoItem()
+            {
+                Id = todoId,
+                Tenant = Tenant,
+                Title = $"GET ITEM {todoId}",
+                Content = $"no content for item {todoId}",
+                Done = ((todoId.ToByteArray()[0] & 1) == 1) ? true : false
+
+            };
+            
+        }
+    }
+}
