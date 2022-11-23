eb7477c5f035971b27317e77af0a03aa3117e5a7
Nicolas CLERC <nicolas.clerc@sablefin.net>
2022-11-23 17:14:57 +0100

    adding help page to debug containerapp

---
+++ v1/src/front-razor/Pages/Help.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using front_common.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text;
+
+namespace front_razor.Pages
+{
+    public class HelpModel : PageModel
+    {
+        private readonly ILogger<HelpModel> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly string _TodoServiceUri;
+        public string ApiEnvironmentVariables { get; set; } = "";
+        public string FrontEnvironmentVariables { get; set; }
+        public HelpModel(ILogger<HelpModel> logger, IConfiguration config)
+        {
+            this._logger = logger;
+            this._configuration = config;
+            this._TodoServiceUri = _configuration.GetValue<string>("TodoServiceUri");
+        }
+
+        public async Task OnGet()
+        {
+            _logger.LogInformation($"TodoServiceUri={_TodoServiceUri}");
+            HttpClient hc = new HttpClient();
+            ApiEnvironmentVariables = await hc.GetStringAsync($"{_TodoServiceUri}/api/Help/Env");
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var evname in Environment.GetEnvironmentVariables().Keys)
+            {
+                sb.AppendLine($"{evname.ToString()}={Environment.GetEnvironmentVariable(evname.ToString())}");
+            }
+            FrontEnvironmentVariables = sb.ToString();
+        }
+    }
+}
---
+++ v1/src/todo-service/Controllers/HelpController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using front_common.Models;
+using todo_service.Services;
+using Newtonsoft.Json;
+using System.Text;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace todo_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelpController : ControllerBase
+    {
+        
+        private readonly IConfiguration _config;
+
+        public HelpController(IConfiguration _config)
+        {
+            
+            this._config = _config;
+        }
+
+        // GET: api/Items
+        /// <summary>
+        /// Return all items 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("env/")]
+        public async Task<string> GetEnv()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(var evname in Environment.GetEnvironmentVariables().Keys)
+            {
+                sb.AppendLine($"{evname.ToString()}={Environment.GetEnvironmentVariable(evname.ToString())}");
+            }
+            return sb.ToString();
+        }
+
+        //[HttpGet("{id}")]
+        //public async Task<string> Get(string id)
+        //{
+        //    return id;
+        //}
+
+        //[HttpPost]
+        //public async void Post([FromBody] string todoJson)
+        //{
+        //    await dataservice.CreateItemAsync(JsonConvert.DeserializeObject<TodoItem>(todoJson));
+        //}
+
+        //[HttpDelete("{id}")]
+        //public async void Delete(Guid id)
+        //{
+        //}
+    }
+}
