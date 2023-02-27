2d4ca8466a3e6f5dc407380842c225f770454166
Nicolas CLERC <nicolas.clerc@sablefin.net>
2023-02-27 17:55:42 +0100

    remove hack on internal api call

--- v1/src/front-razor/Program.cs
+++ v1/src/front-razor/Program.cs
@@ -9,14 +9,15 @@ builder.Services.AddRazorPages();
 var serviceSettings = new front_razor.ServiceSettings();
 
 
-var CONTAINER_APP_ENV_DNS_SUFFIX = System.Environment.GetEnvironmentVariable("CONTAINER_APP_ENV_DNS_SUFFIX");
-if (!string.IsNullOrEmpty(CONTAINER_APP_ENV_DNS_SUFFIX))
-{
-    // HACK we found the CONTAINER_APP_ENV_DNS_SUFFIX en variable -> app is runnnin in container app --> Patch service URL ....
-    Trace.TraceWarning($"Running in CONTAINER APP : patching service URL suffix with .internal.{CONTAINER_APP_ENV_DNS_SUFFIX}");
-    serviceSettings.TodoServiceUri = $"{builder.Configuration.GetValue<string>("TodoServiceUri")}.internal.{CONTAINER_APP_ENV_DNS_SUFFIX}";
-}
-else
+//var CONTAINER_APP_ENV_DNS_SUFFIX = System.Environment.GetEnvironmentVariable("CONTAINER_APP_ENV_DNS_SUFFIX");
+//if (!string.IsNullOrEmpty(CONTAINER_APP_ENV_DNS_SUFFIX))
+//{
+//    // HACK we found the CONTAINER_APP_ENV_DNS_SUFFIX en variable -> app is runnnin in container app --> Patch service URL ....
+//    Trace.TraceWarning($"Running in CONTAINER APP : patching service URL suffix with .internal.{CONTAINER_APP_ENV_DNS_SUFFIX}");
+//    serviceSettings.TodoServiceUri = $"{builder.Configuration.GetValue<string>("TodoServiceUri")}.internal.{CONTAINER_APP_ENV_DNS_SUFFIX}";
+//}
+//else
+
     serviceSettings.TodoServiceUri = builder.Configuration.GetValue<string>("TodoServiceUri");
 
 builder.Services.AddSingleton<front_razor.ServiceSettings>(serviceSettings);
